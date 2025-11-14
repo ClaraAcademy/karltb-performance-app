2c94f9b33973a7db4b01258ddd1bd98c1631b9ad
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-14 14:27:13 +0100

    Make UserSeeder idempotent

--- PerformanceApp.Data/Seeding/Entities/UserSeeder.cs
+++ PerformanceApp.Data/Seeding/Entities/UserSeeder.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using PerformanceApp.Data.Models;
 using PerformanceApp.Data.Seeding.Constants;
 
@@ -8,8 +9,13 @@ public class UserSeeder(UserManager<ApplicationUser> userManager)
 {
     private readonly UserManager<ApplicationUser> _userManager = userManager;
     private static ApplicationUser MapToUser(string username) => new() { UserName = username };
-
     private record Dto(string Username, string Password);
+
+    private async Task<bool> IsPopulated()
+    {
+        var users = await _userManager.Users.AnyAsync();
+        return users;
+    }
 
     private async Task<bool> UserExists(string username)
     {
@@ -20,6 +26,13 @@ public class UserSeeder(UserManager<ApplicationUser> userManager)
 
     public async Task Seed()
     {
+        var exists = await IsPopulated();
+
+        if (exists)
+        {
+            return;
+        }
+
         var dtos = new List<Dto>
         {
             new(UserData.UsernameA, UserData.Password),
@@ -31,8 +44,8 @@ public class UserSeeder(UserManager<ApplicationUser> userManager)
             var username = dto.Username;
             var password = dto.Password;
 
-            var exists = await UserExists(username);
-            if (!exists)
+            var userExists = await UserExists(username);
+            if (!userExists)
             {
                 var user = MapToUser(username);
                 var result = await _userManager.CreateAsync(user, password);
