935c99bd8c9c41c3979c3d3e040bc23cdc1506e3
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-14 14:02:54 +0100

    Make BenchmarkSeeder idempotent

--- PerformanceApp.Data/Seeding/Entities/BenchmarkSeeder.cs
+++ PerformanceApp.Data/Seeding/Entities/BenchmarkSeeder.cs
@@ -10,6 +10,13 @@ public class BenchmarkSeeder(PadbContext context)
     private readonly PortfolioRepository _portfolioRepository = new(context);
     private readonly BenchmarkRepository _benchmarkRepository = new(context);
 
+    private async Task<bool> IsPopulated()
+    {
+        var benchmarks = await _benchmarkRepository.GetBenchmarkMappingsAsync();
+
+        return benchmarks.Any();
+    }
+
     private Benchmark MapToBenchmark((Portfolio, Portfolio) pair)
     {
         var (portfolio, benchmark) = pair;
@@ -22,6 +29,12 @@ public class BenchmarkSeeder(PadbContext context)
 
     public async Task Seed()
     {
+        var exists = await IsPopulated();
+        if (exists)
+        {
+            return;
+        }
+
         var portfolioNames = new List<string> { PortfolioData.PortfolioA, PortfolioData.PortfolioB };
         var benchmarkNames = new List<string> { PortfolioData.BenchmarkA, PortfolioData.BenchmarkB };
 
