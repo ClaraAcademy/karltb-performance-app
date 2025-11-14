a049bd3738ea9eed658257ad3e3f0763cf64145c
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-14 15:25:43 +0100

    Fix wrong username

--- PerformanceApp.Data/Seeding/Constants/UserData.cs
+++ PerformanceApp.Data/Seeding/Constants/UserData.cs
@@ -3,6 +3,6 @@ namespace PerformanceApp.Data.Seeding.Constants;
 public static class UserData
 {
     public static readonly string UsernameA = "UserA";
-    public static readonly string UsernameB = "UserA";
+    public static readonly string UsernameB = "UserB";
     public static readonly string Password = "Password123!";
 }
