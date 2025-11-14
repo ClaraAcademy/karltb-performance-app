77c486797f80c201cf348ca05e43d353b9edc638
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-14 14:03:13 +0100

    Make DateInfoSeeder idempotent

--- PerformanceApp.Data/Seeding/Entities/DateInfoSeeder.cs
+++ PerformanceApp.Data/Seeding/Entities/DateInfoSeeder.cs
@@ -11,11 +11,24 @@ public class DateInfoSeeder(PadbContext context)
     private readonly StagingRepository _stagingRepository = new(context);
     private readonly DateInfoRepository _dateInfoRepository = new(context);
 
+    private async Task<bool> IsPopulated()
+    {
+        var dateInfos = await _dateInfoRepository.GetDateInfosAsync();
+
+        return dateInfos.Any();
+    }
+
     private DateInfo MapToDateInfo(DateOnly bankday) => new DateInfo { Bankday = bankday };
     private DateOnly? GetBankday(Staging staging) => staging.Bankday;
 
     public async Task Seed()
     {
+        var exists = await IsPopulated();
+        if (exists)
+        {
+            return;
+        }
+
         var stagings = await _stagingRepository.GetStagingsAsync();
         var bankdays = stagings.Select(GetBankday)
             .OfType<DateOnly>()
