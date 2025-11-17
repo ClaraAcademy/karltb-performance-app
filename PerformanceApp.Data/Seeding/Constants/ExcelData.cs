225cdb0ca59db300b49f83c44a595c866da78ba7
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-17 11:17:27 +0100

    Add ExcelData constants

---
+++ PerformanceApp.Data/Seeding/Constants/ExcelData.cs
@@ -0,0 +1,6 @@
+namespace PerformanceApp.Data.Seeding.Constants;
+
+public static class ExcelData
+{
+    public static readonly string ExcelFilePath = @"C:\Data\Priser - portföljberäkning.xlsx";
+}
--- PerformanceApp.Data/Seeding/Entities/StagingSeeder.cs
+++ PerformanceApp.Data/Seeding/Entities/StagingSeeder.cs
@@ -1,12 +1,13 @@
 using PerformanceApp.Data.Context;
 using PerformanceApp.Data.Repositories;
+using PerformanceApp.Data.Seeding.Constants;
 
 namespace PerformanceApp.Data.Seeding.Entities;
 
 public class StagingSeeder(PadbContext context)
 {
     private readonly StagingRepository _stagingRepository = new(context);
-    private readonly FileInfo DefaultFile = new(@"C:\Data\Priser - portföljberäkning.xlsx");
+    private readonly FileInfo DefaultFile = new(ExcelData.ExcelFilePath);
 
 
     public async Task Seed(string? filepath = null)
