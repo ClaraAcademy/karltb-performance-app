7cc43de03011283361a1dc88c1f0f2e82276b1e7
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-17 13:42:50 +0100

    Make PerformanceTypeData members public

--- PerformanceApp.Data/Seeding/Constants/PerformanceTypeData.cs
+++ PerformanceApp.Data/Seeding/Constants/PerformanceTypeData.cs
@@ -2,10 +2,10 @@ namespace PerformanceApp.Data.Seeding.Constants;
 
 public static class PerformanceTypeData
 {
-    private static readonly string DayPerformance = "Day Performance";
-    private static readonly string MonthPerformance = "Month Performance";
-    private static readonly string HalfYearPerformance = "Half-Year Performance";
-    private static readonly string CumulativeDayPerformance = "Cumulative Day Performance";
+    public static readonly string DayPerformance = "Day Performance";
+    public static readonly string MonthPerformance = "Month Performance";
+    public static readonly string HalfYearPerformance = "Half-Year Performance";
+    public static readonly string CumulativeDayPerformance = "Cumulative Day Performance";
 
     public static List<string> GetPerformanceTypes()
     {
