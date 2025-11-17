3a2832ab3038d29eab72be7d8cc7d08df1d1218e
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-17 16:17:26 +0100

    Add InstrumentPerformanceRepository test

---
+++ PerformanceApp.Data.Test/Repositories/InstrumentPerformanceTest.cs
@@ -0,0 +1,24 @@
+using PerformanceApp.Data.Models;
+using PerformanceApp.Data.Repositories;
+
+namespace PerformanceApp.Data.Test.Repositories;
+
+public class InstrumentPerformanceTest : RepositoryTest
+{
+    [Fact]
+    public async Task GetInstrumentPerformancesAsync_ReturnsPerformances()
+    {
+        var context = CreateContext();
+        context.InstrumentPerformances.AddRange(new List<InstrumentPerformance>
+        {
+            new InstrumentPerformance { InstrumentId = 1, TypeId = 1, PeriodStart = DateOnly.FromDateTime(DateTime.Now), PeriodEnd = DateOnly.FromDateTime(DateTime.Now), Value = 0.05m },
+            new InstrumentPerformance { InstrumentId = 2, TypeId = 8, PeriodStart = DateOnly.FromDateTime(DateTime.Now), PeriodEnd = DateOnly.FromDateTime(DateTime.Now), Value = 0.10m }
+        });
+        await context.SaveChangesAsync();
+
+        var repo = new InstrumentPerformanceRepository(context);
+        var performances = await repo.GetInstrumentPerformancesAsync();
+
+        Assert.Equal(2, performances.Count());
+    }
+}
