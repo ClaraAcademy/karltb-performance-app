f82b7a614ca1fd3e493666fcf262be534d50af47
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-18 10:42:45 +0100

    Add more Seeding tests

--- PerformanceApp.Data.Test/Seeding/DatabaseSeederTest.cs
+++ PerformanceApp.Data.Test/Seeding/DatabaseSeederTest.cs
@@ -1,56 +1,51 @@
 using Microsoft.EntityFrameworkCore;
 using PerformanceApp.Data.Context;
-
-namespace PerformanceApp.Data.Test.Seeding;
-
-using PerformanceApp.Data.Seeding.Entities;
-using Xunit;
-using System.Threading.Tasks;
 using PerformanceApp.Data.Seeding;
 using Microsoft.AspNetCore.Identity;
 using PerformanceApp.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DatabaseSeederTest
+namespace PerformanceApp.Data.Test.Seeding;
+
+public class DatabaseSeederTest : IDisposable
 {
+    private readonly IServiceScope _scope;
     private readonly PadbContext _context;
-    private readonly DatabaseSeeder _seeder;
-    private readonly UserManager<ApplicationUser> _userManager;
-
-    public static UserManager<ApplicationUser> GetInMemoryUserManager(PadbContext context)
+    private static IServiceProvider GetServiceProvider()
     {
-        var store = new UserStore<ApplicationUser>(context);
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
-        var userManager = new UserManager<ApplicationUser>(
-            store,
-            null, // IOptions<IdentityOptions> optionsAccessor
-            new PasswordHasher<ApplicationUser>(),
-            [],
-            [],
-            null, // ILookupNormalizer keyNormalizer
-            null, // IdentityErrorDescriber errors
-            null, // IServiceProvider services
-            null  // ILogger<UserManager<ApplicationUser>> logger
-        );
-#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
-        return userManager;
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+        var connectionString = config.GetConnectionString("TestContext");
+
+        var services = new ServiceCollection();
+        services.AddDbContext<PadbContext>(options => options.UseSqlServer(connectionString));
+        services.AddIdentityCore<ApplicationUser>().AddEntityFrameworkStores<PadbContext>();
+
+        return services.BuildServiceProvider();
     }
 
     public DatabaseSeederTest()
     {
-        var options = new DbContextOptionsBuilder<PadbContext>()
-            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-            .Options;
-        _context = new PadbContext(options);
-        _userManager = GetInMemoryUserManager(_context);
-        _seeder = new DatabaseSeeder(_context, _userManager);
+        var services = GetServiceProvider();
+        _scope = services.CreateScope();
+        _context = _scope.ServiceProvider.GetRequiredService<PadbContext>();
+        _context.Database.EnsureDeleted();
+        DatabaseInitializer.Initialize(services).GetAwaiter().GetResult();
     }
 
-    public async Task FullDatabaseSeeding_PopulatesAllTables()
+    public void Dispose()
     {
-        // Act
-        await _seeder.Seed();
+        _context.Database.EnsureDeleted();
+        _scope.Dispose();
+        GC.SuppressFinalize(this);
+    }
 
+    [Fact]
+    public void FullDatabaseSeeding_PopulatesAllTables()
+    {
         // Assert
         Assert.NotEmpty(_context.Benchmarks);
         Assert.NotEmpty(_context.DateInfos);
@@ -69,6 +64,52 @@ public class DatabaseSeederTest
         Assert.NotEmpty(_context.Transactions);
         Assert.NotEmpty(_context.TransactionTypes);
         Assert.NotEmpty(_context.PerformanceTypeInfos);
+    }
+
+    [Fact]
+    public async Task FullDatabaseSeeding_IsIdempotent()
+    {
+        var initialBenchmarksCount = _context.Benchmarks.Count();
+        var initialDateInfosCount = _context.DateInfos.Count();
+        var initialInstrumentsCount = _context.Instruments.Count();
+        var initialInstrumentPerformancesCount = _context.InstrumentPerformances.Count();
+        var initialInstrumentPricesCount = _context.InstrumentPrices.Count();
+        var initialInstrumentTypesCount = _context.InstrumentTypes.Count();
+        var initialKeyFigureInfosCount = _context.KeyFigureInfos.Count();
+        var initialKeyFigureValuesCount = _context.KeyFigureValues.Count();
+        var initialPortfoliosCount = _context.Portfolios.Count();
+        var initialPortfolioPerformancesCount = _context.PortfolioPerformances.Count();
+        var initialPortfolioValuesCount = _context.PortfolioValues.Count();
+        var initialPositionsCount = _context.Positions.Count();
+        var initialPositionValuesCount = _context.PositionValues.Count();
+        var initialStagingsCount = _context.Stagings.Count();
+        var initialTransactionsCount = _context.Transactions.Count();
+        var initialTransactionTypesCount = _context.TransactionTypes.Count();
+        var initialPerformanceTypeInfosCount = _context.PerformanceTypeInfos.Count();
+
+        // Re-seed the database
+        var services = GetServiceProvider();
+        await DatabaseInitializer.Initialize(services);
+
+        // Assert that counts remain the same
+        Assert.Equal(initialBenchmarksCount, _context.Benchmarks.Count());
+        Assert.Equal(initialDateInfosCount, _context.DateInfos.Count());
+        Assert.Equal(initialInstrumentsCount, _context.Instruments.Count());
+        Assert.Equal(initialInstrumentPerformancesCount, _context.InstrumentPerformances.Count());
+        Assert.Equal(initialInstrumentPricesCount, _context.InstrumentPrices.Count());
+        Assert.Equal(initialInstrumentTypesCount, _context.InstrumentTypes.Count());
+        Assert.Equal(initialKeyFigureInfosCount, _context.KeyFigureInfos.Count());
+        Assert.Equal(initialKeyFigureValuesCount, _context.KeyFigureValues.Count());
+        Assert.Equal(initialPortfoliosCount, _context.Portfolios.Count());
+        Assert.Equal(initialPortfolioPerformancesCount, _context.PortfolioPerformances.Count());
+        Assert.Equal(initialPortfolioValuesCount, _context.PortfolioValues.Count());
+        Assert.Equal(initialPositionsCount, _context.Positions.Count());
+        Assert.Equal(initialPositionValuesCount, _context.PositionValues.Count());
+        Assert.Equal(initialStagingsCount, _context.Stagings.Count());
+        Assert.Equal(initialTransactionsCount, _context.Transactions.Count());
+        Assert.Equal(initialTransactionTypesCount, _context.TransactionTypes.Count());
+        Assert.Equal(initialPerformanceTypeInfosCount, _context.PerformanceTypeInfos.Count());
+
     }
 
 }
