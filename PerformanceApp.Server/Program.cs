4dfac1b0e7c6cb65e7d6f6ef5849298ad985fed5
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-11-20 14:06:48 +0100

    Prepare Server for integration tests

--- PerformanceApp.Server/Program.cs
+++ PerformanceApp.Server/Program.cs
@@ -121,3 +121,6 @@ app.MapControllers();
 app.MapFallbackToFile("/index.html");
 
 app.Run();
+
+// Make the implicit Program class public so test projects can access it
+public partial class Program { }
