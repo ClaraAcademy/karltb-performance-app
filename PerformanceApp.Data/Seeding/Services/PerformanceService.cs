cab2c9b80ce8e7f54b99a64472e7d3f8ef7e9128
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-02 10:39:24 +0100

    Add PerformanceService to replace GetPErformanceId and related ufns

---
+++ PerformanceApp.Data/Seeding/Services/PerformanceService.cs
@@ -0,0 +1,43 @@
+using PerformanceApp.Data.Repositories;
+using PerformanceApp.Data.Seeding.Constants;
+
+namespace PerformanceApp.Data.Seeding.Services;
+
+public interface IPerformanceService
+{
+    Task<int> GetPerformanceIdAsync(string name);
+    Task<int> GetCumulativeDayPerformanceIdAsync();
+    Task<int> GetDayPerformanceIdAsync();
+    Task<int> GetHalfYearPerformanceIdAsync();
+    Task<int> GetMonthPerformanceIdAsync();
+}
+
+public class PerformanceService(IPerformanceTypeRepository performanceTypeRepository) : IPerformanceService
+{
+    private readonly IPerformanceTypeRepository _performanceTypeRepository = performanceTypeRepository;
+    public async Task<int> GetPerformanceIdAsync(string name)
+    {
+        var performances = await _performanceTypeRepository.GetPerformanceTypeInfosAsync();
+
+        var performance = performances.FirstOrDefault(p => p.Name == name)
+            ?? throw new KeyNotFoundException($"PerformanceType with name '{name}' not found.");
+
+        return performance.Id;
+    }
+    public async Task<int> GetCumulativeDayPerformanceIdAsync()
+    {
+        return await GetPerformanceIdAsync(PerformanceTypeData.CumulativeDayPerformance);
+    }
+    public async Task<int> GetDayPerformanceIdAsync()
+    {
+        return await GetPerformanceIdAsync(PerformanceTypeData.DayPerformance);
+    }
+    public async Task<int> GetHalfYearPerformanceIdAsync()
+    {
+        return await GetPerformanceIdAsync(PerformanceTypeData.HalfYearPerformance);
+    }
+    public async Task<int> GetMonthPerformanceIdAsync()
+    {
+        return await GetPerformanceIdAsync(PerformanceTypeData.MonthPerformance);
+    }
+}
