61a32ef3586f759879f414b4f07fe11b4b1c449c
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-02 12:53:05 +0100

    Add PortfolioService to replace GetPortfolioID ufn

---
+++ PerformanceApp.Data/Seeding/Services/PortfolioService.cs
@@ -0,0 +1,24 @@
+using PerformanceApp.Data.Repositories;
+
+namespace PerformanceApp.Data.Seeding.Services;
+
+public interface IPortfolioService
+{
+    Task<int> GetPortfolioIdAsync(string name);
+}
+
+public class PortfolioService(IPortfolioRepository portfolioRepository) : IPortfolioService
+{
+    private readonly IPortfolioRepository _portfolioRepository = portfolioRepository;
+
+    public async Task<int> GetPortfolioIdAsync(string name)
+    {
+        var portfolios = await _portfolioRepository.GetPortfoliosAsync();
+
+        var portfolio = portfolios.FirstOrDefault(p => p.Name == name)
+            ?? throw new KeyNotFoundException($"Portfolio with name '{name}' not found.");
+
+        return portfolio.Id;
+    }
+
+}
