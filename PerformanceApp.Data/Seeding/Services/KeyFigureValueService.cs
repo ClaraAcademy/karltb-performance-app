2c879e471c3f7abbae5494f151f25275cbb23f02
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-03 10:12:50 +0100

    add UpdateTrackingErrorAsync to replace UpdateTrackingError usp

--- PerformanceApp.Data/Seeding/Services/KeyFigureValueService.cs
+++ PerformanceApp.Data/Seeding/Services/KeyFigureValueService.cs
@@ -7,35 +7,41 @@ namespace PerformanceApp.Data.Seeding.Services;
 public interface IKeyFigureValueService
 {
     Task<bool> UpdateStandardDeviationAsync();
+    Task<bool> UpdateTrackingErrorAsync();
 }
 
 public class KeyFigureValueService : IKeyFigureValueService
 {
     private readonly IKeyFigureValueRepository _keyFigureValueRepository;
     private readonly IKeyFigureInfoService _keyFigureInfoService;
-    private readonly IPerformanceService _performanceService;
+    private readonly IPortfolioRepository _portfolioRepository;
     private readonly IPortfolioPerformanceService _portfolioPerformanceService;
+    private readonly IPerformanceService _performanceService;
     private readonly IDateInfoService _dateInfoService;
     private readonly decimal AnnualizationFactor;
+    private readonly int DayPerformanceId;
 
     public KeyFigureValueService(
         IKeyFigureValueRepository keyFigureValueRepository,
         IKeyFigureInfoService keyFigureInfoService,
-        IPerformanceService performanceService,
+        IPortfolioRepository portfolioRepository,
         IPortfolioPerformanceService portfolioPerformanceService,
+        IPerformanceService performanceService,
         IDateInfoService dateInfoService
     )
     {
         _keyFigureValueRepository = keyFigureValueRepository;
         _keyFigureInfoService = keyFigureInfoService;
-        _performanceService = performanceService;
+        _portfolioRepository = portfolioRepository;
         _portfolioPerformanceService = portfolioPerformanceService;
+        _performanceService = performanceService;
         _dateInfoService = dateInfoService;
         AnnualizationFactor = _dateInfoService.GetAnnualizationFactorAsync().GetAwaiter().GetResult();
+        DayPerformanceId = _performanceService.GetDayPerformanceIdAsync().GetAwaiter().GetResult();
     }
 
     private record Dto(int PortfolioId, int KeyFigureId, decimal Value);
-    private static Dto MapToDto(IGrouping<int, PortfolioPerformance> group, int keyFigureId)
+    private static Dto Aggregate(IGrouping<int, PortfolioPerformance> group, int keyFigureId)
     {
         return new Dto
         (
@@ -43,6 +49,17 @@ public class KeyFigureValueService : IKeyFigureValueService
             keyFigureId,
             ComputeStandardDeviation(
                 group.Select(pp => pp.Value)
+            )
+        );
+    }
+    private static Dto Aggregate(IGrouping<int, Dto> group, int KeyFigureId)
+    {
+        return new Dto
+        (
+            group.Key,
+            KeyFigureId,
+            ComputeStandardDeviation(
+                group.Select(dto => dto.Value)
             )
         );
     }
@@ -89,11 +106,66 @@ public class KeyFigureValueService : IKeyFigureValueService
 
         var keyFigureValues = dayPerformances
             .GroupBy(pp => pp.PortfolioId)
-            .Select(g => MapToDto(g, id))
+            .Select(g => Aggregate(g, id))
             .Select(MapToKeyFigureValue)
             .ToList();
 
         await _keyFigureValueRepository.AddKeyFigureValuesAsync(keyFigureValues);
+        return true;
+    }
+
+    private IEnumerable<PortfolioPerformance> GetPortfolioDayPerformances(Portfolio portfolio)
+    {
+        return portfolio.PortfolioPerformancesNavigation
+            .Where(pp => pp.TypeId == DayPerformanceId);
+    }
+
+    private List<Dto> GetActiveReturn(Portfolio portfolio)
+    {
+        var benchmark = portfolio.BenchmarkPortfoliosNavigation
+            .Select(b => b.BenchmarkPortfolioNavigation)
+            .FirstOrDefault();
+
+        if (benchmark == null)
+        {
+            return [];
+        }
+
+        var portfolioDayPerformances = GetPortfolioDayPerformances(portfolio);
+        var benchmarkDayPerformances = GetPortfolioDayPerformances(benchmark);
+
+        static DateOnly GetKey(PortfolioPerformance pp) => pp.PeriodStart;
+        static Dto Map(PortfolioPerformance pp, PortfolioPerformance bp)
+            => new(pp.PortfolioId, -1, pp.Value - bp.Value);
+
+        var dtos = portfolioDayPerformances
+            .Join(benchmarkDayPerformances, GetKey, GetKey, Map)
+            .ToList();
+
+        return dtos;
+    }
+
+    private List<Dto> GetActiveReturns(List<Portfolio> portfolios)
+    {
+        return portfolios.SelectMany(GetActiveReturn).ToList();
+    }
+
+    public async Task<bool> UpdateTrackingErrorAsync()
+    {
+        var id = await _keyFigureInfoService.GetTrackingErrorIdAsync();
+
+        var actualPortfolios = await _portfolioRepository.GetProperPortfoliosAsync();
+        var portfolios = actualPortfolios.ToList();
+
+        var activeReturns = GetActiveReturns(portfolios);
+
+        var trackingErrors = activeReturns
+            .GroupBy(ar => ar.PortfolioId)
+            .Select(g => Aggregate(g, id))
+            .Select(MapToKeyFigureValue)
+            .ToList();
+
+        await _keyFigureValueRepository.AddKeyFigureValuesAsync(trackingErrors);
         return true;
     }
 }
