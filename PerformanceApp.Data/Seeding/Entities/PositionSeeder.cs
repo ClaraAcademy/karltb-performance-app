99a0e3e9883fce7ce1101fedff6bc7f8fd26f415
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-04 07:35:20 +0100

    Implement native Position-seeding logic

--- PerformanceApp.Data/Seeding/Entities/PositionSeeder.cs
+++ PerformanceApp.Data/Seeding/Entities/PositionSeeder.cs
@@ -1,16 +1,15 @@
 using PerformanceApp.Data.Context;
 using PerformanceApp.Data.Repositories;
-using PerformanceApp.Data.Seeding.Queries;
+using PerformanceApp.Data.Seeding.Services;
 using PerformanceApp.Data.Seeding.Utilities;
 
 namespace PerformanceApp.Data.Seeding.Entities;
 
 public class PositionSeeder(PadbContext context)
 {
-    private readonly PadbContext _context = context;
-
     private readonly DateInfoRepository _dateInfoRepository = new(context);
     private readonly PositionRepository _positionRepository = new(context);
+    private readonly IPositionService _positionService = new PositionService(context);
 
     private async Task<bool> IsPopulated()
     {
@@ -32,9 +31,7 @@ public class PositionSeeder(PadbContext context)
 
         foreach (var bankday in bankdays)
         {
-            var query = PerformanceQueries.UpdatePositions(bankday);
-
-            await SqlExecutor.ExecuteQueryAsync(_context, query);
+            await _positionService.UpdatePositionsAsync(bankday);
         }
     }
 
