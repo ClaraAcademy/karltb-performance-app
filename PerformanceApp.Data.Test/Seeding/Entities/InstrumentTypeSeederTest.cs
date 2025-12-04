c332f5fab628f79731559703364fa8e5c3694b08
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-04 10:04:24 +0100

    Implement InstrumentTypeInfoSeederTest

---
+++ PerformanceApp.Data.Test/Seeding/Entities/InstrumentTypeSeederTest.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using PerformanceApp.Data.Seeding.Constants;
+using PerformanceApp.Data.Seeding.Entities;
+
+namespace PerformanceApp.Data.Test.Seeding.Entities;
+
+[Collection("Seeding collection")]
+public class InstrumentTypeSeederTest : BaseSeederTest
+{
+    private readonly InstrumentTypeSeeder _instrumentTypeSeeder;
+    private readonly StagingSeeder _stagingSeeder;
+
+    public InstrumentTypeSeederTest(DatabaseFixture fixture) : base(fixture)
+    {
+        _instrumentTypeSeeder = new InstrumentTypeSeeder(_context);
+        _stagingSeeder = new StagingSeeder(_context);
+    }
+
+    [Fact]
+    public async Task Seed_AddsInstrumentTypes()
+    {
+        // Arrange
+        var expected = InstrumentTypeData.InstrumentTypes;
+
+        // Act
+        await _stagingSeeder.Seed();
+        await _instrumentTypeSeeder.Seed();
+
+        var instrumentTypes = await _context.InstrumentTypes.ToListAsync();
+        var actual = instrumentTypes
+            .Select(it => it.Name)
+            .OrderBy(n => n)
+            .ToList();
+
+        // Assert
+        Assert.NotEmpty(actual);
+        Assert.Equal(expected.Count, actual.Count);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task Seed_IsIdempotent()
+    {
+        // Arrange
+        await _instrumentTypeSeeder.Seed();
+        var initialCount = await _context.InstrumentTypes.CountAsync();
+
+        // Act
+        await _instrumentTypeSeeder.Seed();
+
+        // Assert
+        var finalCount = await _context.InstrumentTypes.CountAsync();
+        Assert.Equal(initialCount, finalCount);
+    }
+}
--- PerformanceApp.Data/Seeding/Constants/InstrumentTypeData.cs
+++ PerformanceApp.Data/Seeding/Constants/InstrumentTypeData.cs
@@ -5,5 +5,8 @@ public static class InstrumentTypeData
     public const string Stock = "Stock";
     public const string Bond = "Bond";
     public const string Index = "Index";
-    
+
+    private static readonly List<string> _instrumentTypes = [Stock, Bond, Index];
+
+    public static List<string> InstrumentTypes => _instrumentTypes.OrderBy(i => i).ToList();
 }
