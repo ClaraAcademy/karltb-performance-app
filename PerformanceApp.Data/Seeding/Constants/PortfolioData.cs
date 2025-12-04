0d01d3d2acfa73d11dc818a727f5877c2a696d79
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-04 13:16:13 +0100

    Add accessors for Portfolios, Benchmarks, and Both

--- PerformanceApp.Data/Seeding/Constants/PortfolioData.cs
+++ PerformanceApp.Data/Seeding/Constants/PortfolioData.cs
@@ -6,4 +6,25 @@ public static class PortfolioData
     public static readonly string PortfolioB = "Portfolio B";
     public static readonly string BenchmarkA = "Benchmark A";
     public static readonly string BenchmarkB = "Benchmark B";
+
+    private static readonly List<string> _allPortfolios = [
+        PortfolioA,
+        PortfolioB,
+        BenchmarkA,
+        BenchmarkB
+    ];
+
+    private static readonly List<string> _portfolios = [
+        PortfolioA,
+        PortfolioB
+    ];
+
+    private static readonly List<string> _benchmarks = [
+        BenchmarkA,
+        BenchmarkB
+    ];
+
+    public static List<string> AllPortfolios => _allPortfolios.OrderBy(n => n).ToList();
+    public static List<string> Portfolios => _portfolios.OrderBy(n => n).ToList();
+    public static List<string> Benchmarks => _benchmarks.OrderBy(n => n).ToList();
 }
