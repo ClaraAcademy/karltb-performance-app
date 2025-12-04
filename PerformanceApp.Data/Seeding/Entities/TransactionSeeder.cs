41f20fe94a840cde28cdf27e8f734e734cdbdcc4
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-04 07:32:13 +0100

    Implement Transaction-seeding logic

--- PerformanceApp.Data/Seeding/Entities/TransactionSeeder.cs
+++ PerformanceApp.Data/Seeding/Entities/TransactionSeeder.cs
@@ -1,6 +1,10 @@
 using PerformanceApp.Data.Repositories;
 using PerformanceApp.Data.Context;
-using PerformanceApp.Data.Seeding.Queries;
+using PerformanceApp.Data.Seeding.Services;
+using PerformanceApp.Data.Models;
+using PerformanceApp.Data.Seeding.Constants;
+using PerformanceApp.Data.Seeding.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace PerformanceApp.Data.Seeding.Entities;
 
@@ -15,6 +19,22 @@ public class TransactionSeeder(PadbContext context)
         return transactions.Any();
     }
 
+    private async Task<Transaction> Map(TransactionDto dto)
+    {
+        var portfolio = await _context.Portfolios.FirstOrDefaultAsync(p => p.Name == dto.PortfolioName);
+        var instrument = await _context.Instruments.FirstOrDefaultAsync(i => i.Name == dto.InstrumentName);
+
+        return new Transaction
+        {
+            PortfolioNavigation = portfolio,
+            InstrumentNavigation = instrument,
+            Bankday = dto.Bankday,
+            Count = dto.Count,
+            Nominal = dto.Nominal,
+            Proportion = dto.Proportion
+        };
+    }
+
     public async Task Seed()
     {
         var exists = await IsPopulated();
@@ -24,11 +44,15 @@ public class TransactionSeeder(PadbContext context)
             return;
         }
 
-        var queries = TransactionQueries.GetBuyQueries();
 
-        foreach (var q in queries)
+        var dtos = TransactionData.GetInitialTransactions();
+        var transactions = new List<Transaction>();
+        foreach (var dto in dtos)
         {
-            await SqlExecutor.ExecuteQueryAsync(_context, q);
+            var transaction = await Map(dto);
+            transactions.Add(transaction);
         }
+
+        await _transactionRepository.AddTransactionsAsync(transactions);
     }
 }
