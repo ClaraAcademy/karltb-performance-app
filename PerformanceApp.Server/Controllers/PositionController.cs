f36eb4013ca3a88d82386f27a63c5fb563a6e114
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-09 11:11:02 +0100

    Refactor PositionController

--- PerformanceApp.Server/Controllers/PositionController.cs
+++ PerformanceApp.Server/Controllers/PositionController.cs
@@ -3,33 +3,51 @@ using Microsoft.AspNetCore.Mvc;
 using PerformanceApp.Server.Dtos;
 using PerformanceApp.Server.Services;
 
-namespace PerformanceApp.Server.Controllers
+namespace PerformanceApp.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class PositionController(IPositionService service) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize]
-    public class PositionController(IPositionService service) : ControllerBase
+    private readonly IPositionService _service = service;
+
+    private async Task<ActionResult<List<T>>> GetPositionsAsync<T>(
+        Func<DateOnly, int, Task<List<T>>> serviceMethod, int portfolioId, string date
+    )
     {
-        private readonly IPositionService _service = service;
-
-        private ActionResult CheckReturn<T>(List<T>? ps)
-            => ps == null ? NotFound() : Ok(ps);
-
-        // GET: /api/position/stocks&portfolioId=123&date=2025-10-31
-        [HttpGet("stocks")]
-        public async Task<ActionResult<List<StockPositionDTO>>> GetStockPositions([FromQuery] int portfolioId, [FromQuery] string date)
-            => CheckReturn(await _service.GetStockPositionsAsync(DateOnly.Parse(date), portfolioId));
-
-        // GET: /api/position/bonds&portfolioId=123&date=2025-10-31
-        [HttpGet("bonds")]
-        public async Task<ActionResult<List<BondPositionDTO>>> GetBondPositions([FromQuery] int portfolioId, [FromQuery] string date)
-            => CheckReturn(await _service.GetBondPositionsAsync(DateOnly.Parse(date), portfolioId));
-
-        // GET: /api/position/indexes&portfolioId=123&date=2025-10-31
-        [HttpGet("indexes")]
-        public async Task<ActionResult<List<IndexPositionDTO>>> GetIndexPositions([FromQuery] int portfolioId, [FromQuery] string date)
-            => CheckReturn(await _service.GetIndexPositionsAsync(DateOnly.Parse(date), portfolioId));
-
+        var dateOnly = DateOnly.Parse(date);
+        var dtos = await serviceMethod(dateOnly, portfolioId);
+        return Ok(dtos);
     }
 
+    // GET: /api/position/stocks&portfolioId=123&date=2025-10-31
+    [HttpGet("stocks")]
+    public async Task<ActionResult<List<StockPositionDTO>>> GetStockPositions(
+        [FromQuery] int portfolioId, [FromQuery] string date
+    )
+    {
+        var method = _service.GetStockPositionsAsync;
+        return await GetPositionsAsync(method, portfolioId, date);
+    }
+
+    // GET: /api/position/bonds&portfolioId=123&date=2025-10-31
+    [HttpGet("bonds")]
+    public async Task<ActionResult<List<BondPositionDTO>>> GetBondPositions(
+        [FromQuery] int portfolioId, [FromQuery] string date
+    )
+    {
+        var method = _service.GetBondPositionsAsync;
+        return await GetPositionsAsync(method, portfolioId, date);
+    }
+
+    // GET: /api/position/indexes&portfolioId=123&date=2025-10-31
+    [HttpGet("indexes")]
+    public async Task<ActionResult<List<IndexPositionDTO>>> GetIndexPositions(
+        [FromQuery] int portfolioId, [FromQuery] string date
+    )
+    {
+        var method = _service.GetIndexPositionsAsync;
+        return await GetPositionsAsync(method, portfolioId, date);
+    }
 }
