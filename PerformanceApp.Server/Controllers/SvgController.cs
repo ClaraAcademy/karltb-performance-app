85ec0438fee7a338bce970dc2d87cd75a79e4c53
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-10 09:53:09 +0100

    Document SvgController

--- PerformanceApp.Server/Controllers/SvgController.cs
+++ PerformanceApp.Server/Controllers/SvgController.cs
@@ -12,8 +12,25 @@ public class SvgController(ISvgService service) : ControllerBase
     private readonly ISvgService _service = service;
 
 
-    // Get: /api/svg?portfolioId={portfolioID}&width={width}&height={height}
+    /// <summary>
+    /// Gets a cumulative-performance graph in SVG format for a specific portfolio.
+    /// </summary>
+    /// <returns>A string containing the SVG representation of the graph.</returns>
+    /// <param name="portfolioId">The ID of the portfolio for which to create the graph.</param>
+    /// <param name="width">The width of the SVG graph.</param>
+    /// <param name="height">The height of the SVG graph.</param>
+    /// <remarks>
+    /// Sample request:
+    /// <code>
+    /// GET /api/svg?portfolioId=1&amp;width=800&amp;height=600
+    /// Authorization: Bearer {token}
+    /// </code>
+    /// </remarks>
+    /// <response code="200">Returns the SVG graph as a string</response>
+    /// <response code="404">If no graph could be generated for the specified portfolio</response>
     [HttpGet]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<string>> GetCumulativePerformanceGraph(
         [FromQuery] int portfolioId,
         [FromQuery] int? width = null,
