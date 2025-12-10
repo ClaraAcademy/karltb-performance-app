841de61082785dde49c4d25b85774c83dde3e56f
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-10 16:55:04 +0100

    Refactor InstrumentType context configuration

---
+++ PerformanceApp.Data/Context/Configuration/Constants/Entities/InstrumentTypeConstants.cs
@@ -0,0 +1,8 @@
+using PerformanceApp.Data.Models;
+
+namespace PerformanceApp.Data.Context.Configuration.Constants.Entities;
+
+public class InstrumentTypeConstants : EntityConstants<InstrumentType>
+{
+
+}
---
+++ PerformanceApp.Data/Context/Configuration/Entities/InstrumentTypeConfiguration.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PerformanceApp.Data.Context.Configuration.Constants.Entities;
+using PerformanceApp.Data.Models;
+
+namespace PerformanceApp.Data.Context.Configuration.Entities;
+
+using Constants = InstrumentTypeConstants;
+
+public static class InstrumentTypeConfiguration
+{
+    public static void ConfigureInstrumentType(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<InstrumentType>(Configure);
+    }
+
+    static void Configure(EntityTypeBuilder<InstrumentType> entity)
+    {
+        entity.ToTable(Constants.TableName, Constants.DefaultSchema);
+
+        entity.Property(e => e.Id)
+            .HasColumnName(Constants.IdColumnName);
+
+        entity.Property(e => e.Created)
+            .HasColumnName(Constants.CreatedColumnName)
+            .HasDefaultValueSql(Constants.CreatedDefaultValue);
+
+        entity.Property(e => e.Name)
+            .HasColumnName(Constants.NameColumnName);
+    }
+
+}
