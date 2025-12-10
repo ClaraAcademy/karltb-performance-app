582de0014e6bab71acc129c85a1718542633f4c9
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-10 09:15:46 +0100

    Document DateInfoController

--- PerformanceApp.Server/Controllers/DateInfoController.cs
+++ PerformanceApp.Server/Controllers/DateInfoController.cs
@@ -13,7 +13,24 @@ public class DateInfoController(IDateInfoService service) : MyControllerBase
     private readonly IDateInfoService _service = service;
 
     // GET: api/DateInfo
+    /// <summary>
+    /// Gets a list of bankdays in the database.
+    /// </summary>
+    /// <returns>A list of BankdayDTO objects containing the bankdays in the database.</returns>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     GET /api/dateinfo
+    ///     Authorization: Bearer {token}
+    /// 
+    /// </remarks>
+    /// <response code="200">Returns the list of bankdays</response>
+    /// <response code="401">If the user is not authenticated</response>
+    /// <response code="404">If no bankdays are found</response>
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<BankdayDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<IEnumerable<BankdayDTO>>> GetDates()
     {
         if (!UserIsAuthenticated())
