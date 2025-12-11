085f924612d7c943919896bfb2852ebbbd7f11cb
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-11 09:46:53 +0100

    Refactor PositionValue context configuration

---
+++ PerformanceApp.Data/Context/Configuration/Entities/PositionValueConfiguration.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PerformanceApp.Data.Models;
+using PerformanceApp.Data.Context.Configuration.Constants.Entities;
+
+namespace PerformanceApp.Data.Context.Configuration.Entities;
+
+using Constants = PositionValueConstants;
+
+public static class PositionValueConfiguration
+{
+
+    public static void ConfigurePositionValue(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PositionValue>(Configure);
+    }
+    private static void Configure(EntityTypeBuilder<PositionValue> entity)
+    {
+        entity.HasKey(e => new { e.PositionId, e.Bankday });
+
+        entity.ToTable(Constants.TableName, Constants.DefaultSchema);
+
+        entity.Property(e => e.PositionId)
+            .HasColumnName(Constants.PositionIdColumnName);
+
+        entity.Property(e => e.Created)
+            .HasColumnName(Constants.CreatedColumnName)
+            .HasDefaultValueSql(Constants.CreatedDefaultValue);
+
+        entity.Property(e => e.Value)
+            .HasColumnType(Constants.ValueColumnType)
+            .HasColumnName(Constants.ValueColumnName);
+
+        entity.HasOne(d => d.BankdayNavigation)
+            .WithMany(p => p.PositionValuesNavigation)
+            .HasForeignKey(d => d.Bankday)
+            .OnDelete(DeleteBehavior.ClientSetNull)
+            .HasConstraintName(Constants.BankdayForeignKeyName);
+
+        entity.HasOne(d => d.PositionNavigation)
+            .WithMany(p => p.PositionValuesNavigation)
+            .HasForeignKey(d => d.PositionId)
+            .HasConstraintName(Constants.PositionForeignKeyName);
+    }
+}
