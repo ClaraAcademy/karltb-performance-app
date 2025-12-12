11a7e7cb6cea073a03924dc02188642bc103f120
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-12 13:09:48 +0100

    Add KeyFigureValueBuilder

---
+++ PerformanceApp.Server.Test/Builders/KeyFigureValueBuilder.cs
@@ -0,0 +1,34 @@
+using PerformanceApp.Data.Models;
+
+namespace PerformanceApp.Server.Test.Builders;
+
+public class KeyFigureValueBuilder
+{
+    private KeyFigureInfo _keyFigureInfo = new KeyFigureInfoBuilder().Build();
+    private decimal _value = 100.0m;
+    private Portfolio _portfolio = new PortfolioBuilder().Build();
+
+    public KeyFigureValueBuilder WithKeyFigureInfo(KeyFigureInfo keyFigureInfo)
+    {
+        _keyFigureInfo = keyFigureInfo;
+        return this;
+    }
+
+    public KeyFigureValueBuilder WithValue(decimal value)
+    {
+        _value = value;
+        return this;
+    }
+
+    public KeyFigureValue Build()
+    {
+        return new KeyFigureValue
+        {
+            KeyFigureId = _keyFigureInfo.Id,
+            KeyFigureInfoNavigation = _keyFigureInfo,
+            Value = _value,
+            PortfolioId = _portfolio.Id,
+            PortfolioNavigation = _portfolio
+        };
+    }
+}
