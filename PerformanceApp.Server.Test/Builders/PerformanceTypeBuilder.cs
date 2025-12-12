ccf4ed6a31c2506b26736d9fc81070b4715a9a5a
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-12 15:51:19 +0100

    Add PerformanceTypeBuilder and PortfolioPerformanceBuilder

---
+++ PerformanceApp.Server.Test/Builders/PerformanceTypeBuilder.cs
@@ -0,0 +1,50 @@
+using PerformanceApp.Data.Models;
+using PerformanceApp.Server.Test.Builders.Interface;
+
+namespace PerformanceApp.Server.Test.Builders;
+
+public class PerformanceTypeBuilder : IBuilder<PerformanceType>
+{
+    private int _id = 1;
+    private string _name = "Default Performance Type";
+
+    public PerformanceTypeBuilder WithId(int id)
+    {
+        _id = id;
+        return this;
+    }
+
+    public PerformanceTypeBuilder WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    public PerformanceType Build()
+    {
+        return new PerformanceType
+        {
+            Id = _id,
+            Name = _name
+        };
+    }
+
+    public PerformanceType Clone()
+    {
+        return new PerformanceTypeBuilder()
+            .WithId(_id)
+            .WithName(_name)
+            .Build();
+    }
+
+    public IEnumerable<PerformanceType> Many(int count)
+    {
+        for (int i = 1; i <= count; i++)
+        {
+            yield return new PerformanceTypeBuilder()
+                .WithId(i)
+                .WithName($"Performance Type {i}")
+                .Build();
+        }
+    }
+}
---
+++ PerformanceApp.Server.Test/Builders/PortfolioPerformanceBuilder.cs
@@ -0,0 +1,80 @@
+using PerformanceApp.Data.Models;
+using PerformanceApp.Server.Test.Builders.Interface;
+
+namespace PerformanceApp.Server.Test.Builders;
+
+public class PortfolioPerformanceBuilder : IBuilder<PortfolioPerformance>
+{
+    private int _id = 1;
+    private DateOnly _periodStart = DateOnly.FromDateTime(DateTime.UtcNow);
+    private DateOnly _periodEnd = DateOnly.FromDateTime(DateTime.UtcNow);
+    private PerformanceType _performanceType = new PerformanceTypeBuilder().Build();
+    private decimal _value = 0m;
+
+    public PortfolioPerformanceBuilder WithId(int id)
+    {
+        _id = id;
+        return this;
+    }
+
+    public PortfolioPerformanceBuilder WithPeriodStart(DateOnly periodStart)
+    {
+        _periodStart = periodStart;
+        return this;
+    }
+
+    public PortfolioPerformanceBuilder WithPeriodEnd(DateOnly periodEnd)
+    {
+        _periodEnd = periodEnd;
+        return this;
+    }
+
+    public PortfolioPerformanceBuilder WithPerformanceType(PerformanceType performanceType)
+    {
+        _performanceType = performanceType;
+        return this;
+    }
+
+    public PortfolioPerformanceBuilder WithValue(decimal value)
+    {
+        _value = value;
+        return this;
+    }
+
+    public PortfolioPerformance Build()
+    {
+        return new PortfolioPerformance
+        {
+            TypeId = _id,
+            PeriodStart = _periodStart,
+            PeriodEnd = _periodEnd,
+            PerformanceTypeNavigation = _performanceType,
+            Value = _value
+        };
+    }
+
+    public PortfolioPerformance Clone()
+    {
+        return new PortfolioPerformanceBuilder()
+            .WithId(_id)
+            .WithPeriodStart(_periodStart)
+            .WithPeriodEnd(_periodEnd)
+            .WithPerformanceType(_performanceType)
+            .WithValue(_value)
+            .Build();
+    }
+
+    public IEnumerable<PortfolioPerformance> Many(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            yield return new PortfolioPerformanceBuilder()
+                .WithId(_id + i)
+                .WithPeriodStart(_periodStart)
+                .WithPeriodEnd(_periodEnd)
+                .WithPerformanceType(_performanceType)
+                .WithValue(_value)
+                .Build();
+        }
+    }
+}
