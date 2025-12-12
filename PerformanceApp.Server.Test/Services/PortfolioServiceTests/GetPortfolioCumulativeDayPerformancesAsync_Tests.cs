9952d1c5b82b5b760b1207fe24a447e3379637eb
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-12 16:22:24 +0100

    Add GetPortfolioCumulativeDayPerformancesAsync_Tests.cs

---
+++ PerformanceApp.Server.Test/Services/PortfolioServiceTests/GetPortfolioCumulativeDayPerformancesAsync_Tests.cs
@@ -0,0 +1,96 @@
+using Moq;
+using PerformanceApp.Data.Models;
+using PerformanceApp.Data.Seeding.Constants;
+using PerformanceApp.Server.Test.Builders;
+using PerformanceApp.Server.Test.Services.PortfolioServiceTests.Fixture;
+
+namespace PerformanceApp.Server.Test.Services.PortfolioServiceTests;
+
+public class GetPortfolioCumulativeDayPerformancesAsync_Tests() : PortfolioServiceTestFixture()
+{
+    [Fact]
+    public async Task GetPortfolioCumulativeDayPerformanceDTOsAsync_ReturnsExpectedResults()
+    {
+        // Arrange
+        var portfolioId = 1;
+        var portfolioName = "Portfolio 1";
+
+        var performanceType = new PerformanceTypeBuilder()
+            .WithId(5)
+            .WithName(PerformanceTypeData.CumulativeDayPerformance)
+            .Build();
+        var performances = Enumerable.Range(1, 3)
+            .Select(i => new PortfolioPerformanceBuilder()
+                .WithId(i)
+                .WithPeriodStart(new DateOnly(2025, 1, i))
+                .WithPeriodEnd(new DateOnly(2025, 1, i))
+                .WithPerformanceType(performanceType)
+                .WithValue(i * 100m)
+                .Build())
+            .ToList();
+        var portfolio = new PortfolioBuilder()
+            .WithId(portfolioId)
+            .WithName(portfolioName)
+            .WithPerformances(performances)
+            .Build();
+
+        _portfolioRepositoryMock
+            .Setup(r => r.GetPortfolioAsync(portfolioId))
+            .ReturnsAsync(portfolio);
+
+        // Act
+        var actual = await _portfolioService.GetPortfolioCumulativeDayPerformanceDTOsAsync(portfolioId);
+
+        // Assert
+        Assert.Equal(performances.Count, actual.Count);
+        for (int i = 0; i < performances.Count; i++)
+        {
+            Assert.Equal(performances[i].PeriodStart, actual[i].Bankday);
+            Assert.Equal(performances[i].Value, actual[i].Value);
+        }
+    }
+
+    [Fact]
+    public async Task GetPortfolioCumulativeDayPerformanceDTOsAsync_ReturnsEmptyList_WhenNoPerformances()
+    {
+        // Arrange
+        var portfolioId = 1;
+        var portfolioName = "Portfolio 1";
+
+        var portfolio = new PortfolioBuilder()
+            .WithId(portfolioId)
+            .WithName(portfolioName)
+            .WithPerformances([])
+            .Build();
+
+        _portfolioRepositoryMock
+            .Setup(r => r.GetPortfolioAsync(portfolioId))
+            .ReturnsAsync(portfolio);
+
+        // Act
+        var result = await _portfolioService.GetPortfolioCumulativeDayPerformanceDTOsAsync(portfolioId);
+
+        // Assert
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetPortfolioCumulativeDayPerformanceDTOsAsync_ReturnsEmptyList_WhenNoPortfolio()
+    {
+        // Arrange
+        var portfolioId = 1;
+        var portfolio = null as Portfolio;
+
+        _portfolioRepositoryMock
+            .Setup(r => r.GetPortfolioAsync(portfolioId))
+            .ReturnsAsync(portfolio);
+
+        // Act
+        var result = await _portfolioService.GetPortfolioCumulativeDayPerformanceDTOsAsync(portfolioId);
+
+        // Assert
+        Assert.Empty(result);
+    }
+
+
+}
