3d2229793bf2601943967f375a6ae48c0bf871a9
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-15 16:07:32 +0100

    Improve DateInfoServiceTest

--- PerformanceApp.Server.Test/Services/DateInfoServiceTest.cs
+++ PerformanceApp.Server.Test/Services/DateInfoServiceTest.cs
@@ -2,50 +2,55 @@ using PerformanceApp.Data.Models;
 using PerformanceApp.Server.Services;
 using Moq;
 using PerformanceApp.Infrastructure.Repositories;
+using PerformanceApp.Data.Builders;
+using PerformanceApp.Data.Helpers;
 
 namespace PerformanceApp.Server.Test.Services;
 
 public class DateInfoServiceTest
 {
     private readonly Mock<IDateInfoRepository> _mockRepo;
+    private readonly DateInfoService _service;
 
     public DateInfoServiceTest()
     {
         _mockRepo = new Mock<IDateInfoRepository>();
+        _service = new DateInfoService(_mockRepo.Object);
     }
+
     [Fact]
     public async Task GetBankdayDTOsAsync_ReturnsCorrectData()
     {
         // Arrange
-        var bankdays = new List<DateInfo>
-        {
-            new DateInfo { Bankday = new DateOnly(2023, 1, 1) },
-            new DateInfo { Bankday = new DateOnly(2023, 1, 2) }
-        };
+        var n = 3;
+        var dateInfos = new DateInfoBuilder()
+            .Many(n)
+            .ToList();
 
-        _mockRepo.Setup(repo => repo.GetDateInfosAsync()).ReturnsAsync(bankdays);
-
-        var service = new DateInfoService(_mockRepo.Object);
+        _mockRepo
+            .Setup(repo => repo.GetDateInfosAsync())
+            .ReturnsAsync(dateInfos);
 
         // Act
-        var result = await service.GetBankdayDTOsAsync();
+        var result = await _service.GetBankdayDTOsAsync();
 
         // Assert
-        Assert.Equal(2, result.Count);
-        Assert.Contains(result, dto => dto.Bankday == new DateOnly(2023, 1, 1));
-        Assert.Contains(result, dto => dto.Bankday == new DateOnly(2023, 1, 2));
+        Assert.Equal(n, result.Count);
+        var eSorted = dateInfos.OrderedBankdays();
+        var aSorted = result.OrderedBankdays();
+        Assert.Equal(eSorted, aSorted);
     }
 
     [Fact]
     public async Task GetBankdayDTOsAsync_HandlesEmptyData()
     {
         // Arrange
-        _mockRepo.Setup(repo => repo.GetDateInfosAsync()).ReturnsAsync(new List<DateInfo>());
-
-        var service = new DateInfoService(_mockRepo.Object);
+        _mockRepo
+            .Setup(repo => repo.GetDateInfosAsync())
+            .ReturnsAsync([]);
 
         // Act
-        var result = await service.GetBankdayDTOsAsync();
+        var result = await _service.GetBankdayDTOsAsync();
 
         // Assert
         Assert.Empty(result);
