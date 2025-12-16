a6ca4ec11a4e65603edeb690675e544212e027c2
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-16 10:07:21 +0100

    Refactor BenchmarkRepositoryTest

--- PerformanceApp.Infrastructure.Test/Repositories/BenchmarkRepositoryTest.cs
+++ PerformanceApp.Infrastructure.Test/Repositories/BenchmarkRepositoryTest.cs
@@ -1,4 +1,4 @@
-using PerformanceApp.Data.Models;
+using PerformanceApp.Data.Builders;
 using PerformanceApp.Infrastructure.Repositories;
 
 namespace PerformanceApp.Infrastructure.Test.Repositories;
@@ -6,24 +6,6 @@ namespace PerformanceApp.Infrastructure.Test.Repositories;
 public class BenchmarkRepositoryTest : BaseRepositoryTest
 {
     private readonly BenchmarkRepository _repository;
-
-    private static List<Benchmark> CreateBenchmarks()
-    {
-        return [
-            new Benchmark { PortfolioId = 1, BenchmarkId = 3 },
-            new Benchmark { PortfolioId = 2, BenchmarkId = 4 }
-        ];
-    }
-
-    private static List<Portfolio> CreatePortfolios()
-    {
-        return [
-            new Portfolio { Id = 1, Name = "Portfolio 1" },
-            new Portfolio { Id = 2, Name = "Portfolio 2" },
-            new Portfolio { Id = 3, Name = "Portfolio 3" },
-            new Portfolio { Id = 4, Name = "Portfolio 4" }
-        ];
-    }
 
     public BenchmarkRepositoryTest()
     {
@@ -33,31 +15,55 @@ public class BenchmarkRepositoryTest : BaseRepositoryTest
     [Fact]
     public async Task AddBenchmarkMappingsAsync_AddsBenchmarksToDatabase()
     {
-        var benchmarks = CreateBenchmarks();
+        // Arrange
+        var expected = new BenchmarkBuilder()
+            .Many(3)
+            .ToList();
 
-        await _repository.AddBenchmarkMappingsAsync(benchmarks);
+        // Act
+        await _repository.AddBenchmarkMappingsAsync(expected);
 
-        var retrievedBenchmark1 = await _context.Benchmarks.FindAsync(1, 3);
-        var retrievedBenchmark2 = await _context.Benchmarks.FindAsync(2, 4);
+        var actual = _context
+            .Benchmarks
+            .ToList();
 
-        Assert.NotNull(retrievedBenchmark1);
-        Assert.NotNull(retrievedBenchmark2);
+        // Assert
+        Assert.Equal(expected.Count, actual.Count);
+        for (int i = 0; i < expected.Count; i++)
+        {
+            var e = expected[i];
+            var a = actual[i];
+
+            Assert.Equal(e.PortfolioId, a.PortfolioId);
+            Assert.Equal(e.BenchmarkId, a.BenchmarkId);
+        }
     }
 
     [Fact]
     public async Task GetBenchmarkMappingsAsync_ReturnsAllBenchmarks()
     {
-        _context.Portfolios.AddRange(CreatePortfolios());
+        // Arrange
+        var expected = new BenchmarkBuilder()
+            .Many(5)
+            .ToList();
 
-        var benchmarks = CreateBenchmarks();
-
-        _context.Benchmarks.AddRange(benchmarks);
+        await _context
+            .Benchmarks
+            .AddRangeAsync(expected);
         await _context.SaveChangesAsync();
 
-        var retrievedBenchmarks = await _repository.GetBenchmarkMappingsAsync();
+        // Act
+        var actual = await _repository.GetBenchmarkMappingsAsync();
 
-        Assert.Equal(benchmarks.Count, retrievedBenchmarks.Count());
+        // Assert
+        Assert.Equal(expected.Count, actual.Count());
+        for (int i = 0; i < expected.Count; i++)
+        {
+            var e = expected[i];
+            var a = actual.ElementAt(i);
+
+            Assert.Equal(e.PortfolioId, a.PortfolioId);
+            Assert.Equal(e.BenchmarkId, a.BenchmarkId);
+        }
     }
-
-
 }
