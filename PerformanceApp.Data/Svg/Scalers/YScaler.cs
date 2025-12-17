07a2e296fcdc8e4f1caa00449e37446269a1d362
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-17 13:21:52 +0100

    Change wrong method name in YScaler

--- PerformanceApp.Data/Svg/Scalers/YScaler.cs
+++ PerformanceApp.Data/Svg/Scalers/YScaler.cs
@@ -7,7 +7,7 @@ public class YScaler
     private readonly float _max;
     private readonly float _min;
 
-    public float ScaleY(float y)
+    public float Scale(float y)
     {
         return _margin + (_max - y) * (_height - 2 * _margin) / (_max - _min);
     }
