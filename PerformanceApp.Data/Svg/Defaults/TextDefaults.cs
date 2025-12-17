6623f1886089acc5a68308a066a3f8f8182873d0
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-17 11:37:56 +0100

    Add TextFactory

---
+++ PerformanceApp.Data/Svg/Defaults/TextDefaults.cs
@@ -0,0 +1,6 @@
+namespace PerformanceApp.Data.Svg.Defaults;
+
+public class TextDefaults
+{
+    public static int FontSize => 12;
+}
---
+++ PerformanceApp.Data/Svg/Factories/TextFactory.cs
@@ -0,0 +1,36 @@
+using System.Xml.Linq;
+using PerformanceApp.Data.Svg.Constants;
+using PerformanceApp.Data.Svg.Defaults;
+using PerformanceApp.Data.Svg.Formatters;
+
+namespace PerformanceApp.Data.Svg.Factories;
+
+public class TextFactory(string anchor, float angle, int size)
+{
+    private readonly string _anchor = anchor;
+    private readonly float _angle = angle;
+    private readonly int _size = size;
+    private readonly DecimalFormatter<float> _decimalFormatter = new();
+
+    public TextFactory() : this("middle", 0, TextDefaults.FontSize) { }
+
+    string Rotate(string x, string y) => $"rotate({_angle} {x},{y})";
+
+    public XElement Create(string text, float x, float y)
+    {
+        var fx = _decimalFormatter.Format(x);
+        var fy = _decimalFormatter.Format(y);
+
+        var attributes = new List<(string, string)>
+        {
+            (XAttributeConstants.X, fx),
+            (XAttributeConstants.Y, fy),
+            (XAttributeConstants.FontSize, _size.ToString()),
+            (XAttributeConstants.TextAnchor, _anchor),
+            (XAttributeConstants.Transform, Rotate(fx, fy))
+        };
+
+        return XElementFactory.Create(XElementConstants.Text, text, attributes);
+    }
+
+}
