33684b30145e539041ca2f35fd89bf3a03859b88
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-17 14:59:32 +0100

    Update scalers

--- PerformanceApp.Data/Svg/Scalers/XScaler.cs
+++ PerformanceApp.Data/Svg/Scalers/XScaler.cs
@@ -4,10 +4,22 @@ public class XScaler(int width, int margin, int numberOfPoints)
 {
     private readonly int _width = width;
     private readonly int _margin = margin;
+    public int Width => _width;
     private readonly int _numberOfPoints = numberOfPoints;
 
     public float Scale(float x)
     {
-        return _margin + x * (_width - 2 * _margin) / _numberOfPoints;
+        return _margin + (x * (_width - (2 * _margin)) / _numberOfPoints);
+    }
+    public float Scale(int x) => Scale((float)x);
+
+    public List<float> Scale(IEnumerable<float> xs)
+    {
+        return xs.Select(Scale).ToList();
+    }
+
+    public List<float> Scale(IEnumerable<int> xs)
+    {
+        return xs.Select(Scale).ToList();
     }
 }
--- PerformanceApp.Data/Svg/Scalers/YScaler.cs
+++ PerformanceApp.Data/Svg/Scalers/YScaler.cs
@@ -6,8 +6,21 @@ public class YScaler(int height, int margin, float max, float min)
     private readonly int _margin = margin;
     private readonly float _max = max;
     private readonly float _min = min;
+    public int Height => _height;
+
     public float Scale(float y)
     {
         return _margin + (_max - y) * (_height - 2 * _margin) / (_max - _min);
+    }
+    public float Scale(int y) => Scale((float)y);
+
+    public List<float> Scale(IEnumerable<float> ys)
+    {
+        return ys.Select(Scale).ToList();
+    }
+
+    public List<float> Scale(IEnumerable<int> ys)
+    {
+        return ys.Select(Scale).ToList();
     }
 }
