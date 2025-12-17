525132cb5cab5680117e1809bc8cf1ff568f3368
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-17 13:25:43 +0100

    Add constructor to YScaler

--- PerformanceApp.Data/Svg/Scalers/YScaler.cs
+++ PerformanceApp.Data/Svg/Scalers/YScaler.cs
@@ -1,12 +1,11 @@
 namespace PerformanceApp.Data.Svg.Scalers;
 
-public class YScaler
+public class YScaler(int height, int margin, float max, float min)
 {
-    private readonly float _height;
-    private readonly float _margin;
-    private readonly float _max;
-    private readonly float _min;
-
+    private readonly int _height = height;
+    private readonly int _margin = margin;
+    private readonly float _max = max;
+    private readonly float _min = min;
     public float Scale(float y)
     {
         return _margin + (_max - y) * (_height - 2 * _margin) / (_max - _min);
