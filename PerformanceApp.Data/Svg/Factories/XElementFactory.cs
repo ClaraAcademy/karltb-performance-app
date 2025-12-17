23e8f702cea38ee7eb50eac6926578d056819bac
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-17 10:16:07 +0100

    Add XElementFactory

---
+++ PerformanceApp.Data/Svg/Factories/XElementFactory.cs
@@ -0,0 +1,12 @@
+using System.Xml.Linq;
+
+namespace PerformanceApp.Data.Svg.Factories;
+
+public class XElementFactory
+{
+    private static readonly string SvgNamespace = "http://www.w3.org/2000/svg";
+    public static XElement Create(string name, params XAttribute[] attributes)
+    {
+        return new XElement($"{SvgNamespace}{name}", attributes);
+    }
+}
