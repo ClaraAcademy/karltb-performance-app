db45e65a5e97756f7a84efc51a74398a3e976c59
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-17 11:08:58 +0100

    Remove unused constants

--- PerformanceApp.Data/Svg/Constants/LineConstants.cs
+++ PerformanceApp.Data/Svg/Constants/LineConstants.cs
@@ -2,17 +2,6 @@ namespace PerformanceApp.Data.Svg.Constants;
 
 public static class LineConstants
 {
-    public static List<string> AttributeNames => [
-        XAttributeConstants.X1,
-        XAttributeConstants.Y1,
-        XAttributeConstants.X2,
-        XAttributeConstants.Y2,
-        XAttributeConstants.Stroke,
-        XAttributeConstants.StrokeWidth
-    ];
-
     public static string ElementName => "line";
-
-    public static string DefaultColor => "black";
     public static int DefaultWidth => 1;
 }
