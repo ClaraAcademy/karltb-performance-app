9fa605eb99d2ca21d356d7df4dee20197b577c3f
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-17 13:01:53 +0100

    add SvgUtilities

--- PerformanceApp.Data/Svg/Utilities/SvgUtilities.cs
+++ PerformanceApp.Data/Svg/Utilities/SvgUtilities.cs
@@ -5,10 +5,16 @@ namespace PerformanceApp.Data.Svg.Utilities;
 public static class SvgUtilities
 {
     private static readonly DecimalFormatter<float> _decimalFormatter = new();
+
     public static string MapToPoint(float x, float y)
     {
         var fx = _decimalFormatter.Format(x);
         var fy = _decimalFormatter.Format(y);
         return $"{fx},{fy}";
+    }
+
+    public static string MapToString(List<string> points)
+    {
+        return string.Join(" ", points);
     }
 }
