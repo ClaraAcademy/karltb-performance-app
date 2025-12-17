3b51df39d4e4bc5f3ec5e27c975f1f48e0407ccb
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-17 13:01:23 +0100

    Update TextFactory

--- PerformanceApp.Data/Svg/Factories/TextFactory.cs
+++ PerformanceApp.Data/Svg/Factories/TextFactory.cs
@@ -13,6 +13,8 @@ public class TextFactory(string anchor, float angle, int size)
     private readonly DecimalFormatter<float> _decimalFormatter = new();
 
     public TextFactory() : this("middle", 0, TextDefaults.FontSize) { }
+    public TextFactory(float angle) : this("middle", angle, TextDefaults.FontSize) { }
+    public TextFactory(string anchor, float angle) : this(anchor, angle, TextDefaults.FontSize) { }
 
     string Rotate(string x, string y) => $"rotate({_angle} {x},{y})";
 
