331974ccd2d4ad3975b40450140294bc6eae27aa
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2025-12-18 15:43:00 +0100

    Update PolyLineFactory

--- PerformanceApp.Data/Svg/Factories/Core/PolyLineFactory.cs
+++ PerformanceApp.Data/Svg/Factories/Core/PolyLineFactory.cs
@@ -5,23 +5,24 @@ using PerformanceApp.Data.Svg.Utilities;
 
 namespace PerformanceApp.Data.Svg.Factories.Core;
 
-public class PolyLineFactory(string color, int width, bool dotted)
+public class PolyLineFactory
 {
-    private readonly string _color = color;
-    private readonly int _width = width;
-    private readonly bool _dotted = dotted;
-
-    public XElement GetSvgPolyline(List<string> points)
+    public static XElement GetSvgPolyline(
+        List<string> points, 
+        string color = ColorConstants.Black, 
+        int width = 1, 
+        bool dotted = false
+    )
     {
         var joined = SvgUtilities.MapToString(points);
 
         var element = new XElementBuilder(XElementConstants.Polyline)
             .WithAttribute(XAttributeConstants.Points, joined)
             .WithAttribute(XAttributeConstants.Fill, XAttributeConstants.None)
-            .WithAttribute(XAttributeConstants.Stroke, _color)
-            .WithAttribute(XAttributeConstants.StrokeWidth, _width);
+            .WithAttribute(XAttributeConstants.Stroke, color)
+            .WithAttribute(XAttributeConstants.StrokeWidth, width);
 
-        if (_dotted)
+        if (dotted)
         {
             var spacing = SvgUtilities.MapToPoint(2, 2);
             element = element
