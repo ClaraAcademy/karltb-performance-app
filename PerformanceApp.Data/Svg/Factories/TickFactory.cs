aa5c0e865bb6612050ae3eb7855b82fbc7dc63cc
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2026-01-05 15:04:19 +0100

    Remove unused methods

--- PerformanceApp.Data/Svg/Factories/TickFactory.cs
+++ PerformanceApp.Data/Svg/Factories/TickFactory.cs
@@ -30,29 +30,9 @@ public class TickFactory
         _lineFactory = new LineFactory(_color, 1);
     }
 
-    public TickFactory(string color, XScaler xScaler, YScaler yScaler)
-    {
-        _xScaler = xScaler;
-        _yScaler = yScaler;
-        _color = color;
-        _lineFactory = new LineFactory(_color, 1);
-    }
-
     public XElement Create(float x1, float y1, float x2, float y2)
     {
         return _lineFactory.Create(x1, y1, x2, y2);
-    }
-
-    public IEnumerable<XElement> CreateXs(int count, float y)
-    {
-        var y1 = y - _offset;
-        var y2 = y + _offset;
-        var xs = Enumerable
-            .Range(1, count)
-            .Select(_xScaler.Scale)
-            .Select(x => (float)x);
-
-        return xs.Select(x => Create(x, y1, x, y2));
     }
 
     public IEnumerable<XElement> CreateXs(IEnumerable<float> xs, float y)
@@ -61,18 +41,6 @@ public class TickFactory
         var y2 = y + _offset;
 
         return xs.Select(x => Create(x, y1, x, y2));
-    }
-
-    public IEnumerable<XElement> CreateYs(int count, float x)
-    {
-        var x1 = x - _offset;
-        var x2 = x + _offset;
-        var ys = Enumerable
-            .Range(1, count)
-            .Select(_yScaler.Scale)
-            .Select(y => (float)y);
-
-        return ys.Select(y => Create(x1, y, x2, y));
     }
 
     public IEnumerable<XElement> CreateYs(IEnumerable<float> ys, float x)
