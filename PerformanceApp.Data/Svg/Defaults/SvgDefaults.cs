cb81a5bfb4a8d18229caf095e4739a1dde4ca07a
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2026-01-05 11:23:19 +0100

    Update SVG Defaults

---
+++ PerformanceApp.Data/Svg/Defaults/SvgDefaults.cs
@@ -0,0 +1,9 @@
+namespace PerformanceApp.Data.Svg.Defaults;
+
+public class SvgDefaults
+{
+    public const int XMargin = 50;
+    public const int YMargin = 50;
+    public const string PrimaryColor = "#211f5e";
+    public const string SecondaryColor = "#ec646b";
+}
