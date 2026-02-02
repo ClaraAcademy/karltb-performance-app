56ad55c6e40edfde040fa4b0af45e451934666d2
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2026-02-02 15:53:33 +0100

    Unit test AxisFactory

--- PerformanceApp.Data.Test/Svg/Factories/AxisFactoryTest.cs
+++ PerformanceApp.Data.Test/Svg/Factories/AxisFactoryTest.cs
@@ -1,47 +1,65 @@
 using System.Xml.Linq;
+using Moq;
+using PerformanceApp.Data.Svg.Builders.Interfaces;
 using PerformanceApp.Data.Svg.Factories;
-using PerformanceApp.Data.Svg.Formatters;
 
 namespace PerformanceApp.Data.Test.Svg.Factories;
 
 public class AxisFactoryTest
 {
-    private readonly Func<float, string> _format = DecimalFormatter.Format;
+    class TestAxisBuilder : IAxisBuilder
+    {
+        private float _x1, _y1, _x2, _y2;
+
+        public IAxisBuilder From(float x1, float y1) { _x1 = x1; _y1 = y1; return this; }
+        public IAxisBuilder To(float x2, float y2) { _x2 = x2; _y2 = y2; return this; }
+        public XElement Build()
+        {
+            return new XElement("line",
+                new XAttribute("x1", _x1),
+                new XAttribute("y1", _y1),
+                new XAttribute("x2", _x2),
+                new XAttribute("y2", _y2));
+        }
+    }
     [Fact]
-    public void X_Property_ShouldReturnXAxisElement()
+    public void X_ShouldReturnXAxisLine()
     {
         // Arrange
-        float x0 = 10, y0 = 20, nX = 100, nY = 200;
-        var factory = new AxisFactory(x0, y0, nX, nY);
+        float x0 = 10;
+        float y0 = 20;
+        float nX = 200;
+        float nY = 150;
+        var axisFactory = new AxisFactory(new TestAxisBuilder(), x0, y0, nX, nY);
 
         // Act
-        XElement xAxis = factory.X;
+        var xAxis = axisFactory.X;
 
         // Assert
         Assert.NotNull(xAxis);
-        Assert.Equal("line", xAxis.Name.LocalName);
-        Assert.Equal(_format(0), xAxis.Attribute("x1")?.Value);
-        Assert.Equal(_format(y0), xAxis.Attribute("y1")?.Value);
-        Assert.Equal(_format(nX), xAxis.Attribute("x2")?.Value);
-        Assert.Equal(_format(y0), xAxis.Attribute("y2")?.Value);
+        Assert.Equal(0f.ToString(), xAxis.Attribute("x1")?.Value);
+        Assert.Equal(y0.ToString(), xAxis.Attribute("y1")?.Value);
+        Assert.Equal(nX.ToString(), xAxis.Attribute("x2")?.Value);
+        Assert.Equal(y0.ToString(), xAxis.Attribute("y2")?.Value);
     }
-
     [Fact]
-    public void Y_Property_ShouldReturnYAxisElement()
+    public void Y_ShouldReturnYAxisLine()
     {
         // Arrange
-        float x0 = 10, y0 = 20, nX = 100, nY = 200;
-        var factory = new AxisFactory(x0, y0, nX, nY);
+        float x0 = 10;
+        float y0 = 20;
+        float nX = 200;
+        float nY = 150;
+        var axisFactory = new AxisFactory(new TestAxisBuilder(), x0, y0, nX, nY);
 
         // Act
-        XElement yAxis = factory.Y;
+        var yAxis = axisFactory.Y;
 
         // Assert
         Assert.NotNull(yAxis);
-        Assert.Equal("line", yAxis.Name.LocalName);
-        Assert.Equal(_format(x0), yAxis.Attribute("x1")?.Value);
-        Assert.Equal(_format(0), yAxis.Attribute("y1")?.Value);
-        Assert.Equal(_format(x0), yAxis.Attribute("x2")?.Value);
-        Assert.Equal(_format(nY), yAxis.Attribute("y2")?.Value);
+        Assert.Equal(x0.ToString(), yAxis.Attribute("x1")?.Value);
+        Assert.Equal(0f.ToString(), yAxis.Attribute("y1")?.Value);
+        Assert.Equal(x0.ToString(), yAxis.Attribute("x2")?.Value);
+        Assert.Equal(nY.ToString(), yAxis.Attribute("y2")?.Value);
     }
 }
--- PerformanceApp.Data/Svg/Factories/AxisFactory.cs
+++ PerformanceApp.Data/Svg/Factories/AxisFactory.cs
@@ -1,10 +1,17 @@
 using System.Xml.Linq;
 using PerformanceApp.Data.Svg.Builders;
+using PerformanceApp.Data.Svg.Builders.Interfaces;
 
 namespace PerformanceApp.Data.Svg.Factories;
 
-public class AxisFactory(float x0, float y0, float nX, float nY)
+public class AxisFactory(IAxisBuilder axisBuilder, float x0, float y0, float nX, float nY)
 {
-    public XElement X => AxisBuilder.Build((0, y0), (nX, y0));
-    public XElement Y => AxisBuilder.Build((x0, 0), (x0, nY));
+    public static AxisFactory Create(float x0, float y0, float nX, float nY)
+    {
+        var builder = new AxisBuilder();
+        return new(builder, x0, y0, nX, nY);
+    }
+
+    public XElement X => axisBuilder.From(0, y0).To(nX, y0).Build();
+    public XElement Y => axisBuilder.From(x0, 0).To(x0, nY).Build();
 }
