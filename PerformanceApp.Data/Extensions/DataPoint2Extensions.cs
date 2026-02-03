53390be45ce23c81764e3702427d842f09835e26
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2026-02-03 16:18:26 +0100

    Update DataPoint2Extensions

--- PerformanceApp.Data/Extensions/DataPoint2Extensions.cs
+++ PerformanceApp.Data/Extensions/DataPoint2Extensions.cs
@@ -1,25 +1,29 @@
 using PerformanceApp.Data.Dtos;
+using PerformanceApp.Data.Svg.Common;
 
 namespace PerformanceApp.Data.Extensions;
 
 public static class DataPoint2Extensions
 {
-    public static List<float> GetY1s(this IEnumerable<DataPoint2> dataPoints)
+    public static ChartData ToChartData(this IEnumerable<DataPoint2> dataPoints, string color1, string color2)
     {
-        return dataPoints.Select(dp => dp.Y1).ToList();
+        var xs = dataPoints.GetXs().Select(d => d.ToString()).ToList();
+        var ys = dataPoints.ToSeriesY(color1, color2);
+        return new ChartData(xs, ys);
     }
-    public static List<float> GetY2s(this IEnumerable<DataPoint2> dataPoints)
+
+    public static ChartSeries ToSeriesY1(this IEnumerable<DataPoint2> dataPoints, string color) => dataPoints.ToSeriesY(d => d.Y1, color);
+    public static ChartSeries ToSeriesY2(this IEnumerable<DataPoint2> dataPoints, string color) => dataPoints.ToSeriesY(d => d.Y2, color);
+    private static ChartSeries ToSeriesY(this IEnumerable<DataPoint2> dataPoints, Func<DataPoint2, float> selector, string color)
     {
-        return dataPoints.Select(dp => dp.Y2).ToList();
+        return ChartSeries.From(dataPoints, selector, color);
+    }
+    public static List<ChartSeries> ToSeriesY(this IEnumerable<DataPoint2> dataPoints, string color1, string color2)
+    {
+        return [dataPoints.ToSeriesY1(color1), dataPoints.ToSeriesY2(color2)];
     }
     public static List<DateOnly> GetXs(this IEnumerable<DataPoint2> dataPoints)
     {
         return dataPoints.Select(dp => dp.X).ToList();
-    }
-    public static List<float> GetYs(this IEnumerable<DataPoint2> dataPoints)
-    {
-        var y1s = dataPoints.GetY1s();
-        var y2s = dataPoints.GetY2s();
-        return y1s.Concat(y2s).ToList();
     }
 }
