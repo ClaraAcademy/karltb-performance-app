a831e01dd21d243ad588039bed5373c68b31bb6b
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2026-02-06 13:45:27 +0100

    Update Report component

--- performanceapp.client/src/components/Report/Report.tsx
+++ performanceapp.client/src/components/Report/Report.tsx
@@ -1,14 +1,15 @@
+const ANCHOR_URL =
+  "https://app.powerbi.com/view?r=eyJrIjoiYzRmZDBiZWQtMzZjOC00YjJiLWI4ZmEtNzY1MTY3YmQwZTc4IiwidCI6IjUxZmZiOGE1LWZmZGUtNDQ5ZC05OWYzLTg5ZDc1OWFkOTI4NSIsImMiOjl9";
+const ANCHOR_TARGET = "_blank";
+const ANCHOR_REL = "noopener noreferrer";
+
 const Report = () => {
   return (
     <div>
       <h2>Power BI</h2>
       <p>
         The PowerBI report is available{" "}
-        <a
-          href="https://app.powerbi.com/view?r=eyJrIjoiYzRmZDBiZWQtMzZjOC00YjJiLWI4ZmEtNzY1MTY3YmQwZTc4IiwidCI6IjUxZmZiOGE1LWZmZGUtNDQ5ZC05OWYzLTg5ZDc1OWFkOTI4NSIsImMiOjl9"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href={ANCHOR_URL} target={ANCHOR_TARGET} rel={ANCHOR_REL}>
           here
         </a>
         .
