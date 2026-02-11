68cd941fb7ce7fc218a898fd66f406cc1b29acc0
Karl Törnblom Bartholf <karl.tornblom-bartholf@claraconsulting.se>
2026-02-11 15:49:41 +0100

    Remove unused contexts

--- performanceapp.client/src/main.tsx
+++ performanceapp.client/src/main.tsx
@@ -2,21 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./pages/App.tsx";
-import { BankdayProvider } from "./contexts/BankdayContext.tsx";
-import { PortfolioProvider } from "./contexts/PortfolioContext.tsx";
-import { PortfolioBenchmarkProvider } from "./contexts/PortfolioBenchmarkContext.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <BankdayProvider>
-        <PortfolioProvider>
-          <PortfolioBenchmarkProvider>
             <App />
-          </PortfolioBenchmarkProvider>
-        </PortfolioProvider>
-      </BankdayProvider>
     </AuthProvider>
   </StrictMode>,
 );
